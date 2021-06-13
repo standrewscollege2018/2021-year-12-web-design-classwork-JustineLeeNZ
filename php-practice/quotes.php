6ed5bbf8d0cb6b7206283a1845779ade50a20446
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-13 20:41:12 +1200

    Output practice
    Practice using echo

---
+++ php-practice/quotes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Practice using ECHO</title>
+  </head>
+  <body>
+    <?php
+    echo "<p>I will build a great, great wall on our southern border, and
+      I will have Mexico pay for that wall. Mark my words.</p>";
+      
+    echo "<p>It's really cold outside, they are calling it a major freeze,
+    weeks ahead of normal. Man, we could use a big fat dose of global
+    warming!</p>";
+     ?>
+
+  </body>
+</html>
