aa290909af783a4be3cd8f8665bb979d30eb1ecc
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-13 21:32:56 +1200

    Practice variables

---
+++ php-practice/silly_story.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Silly Story</title>
+  </head>
+  <body>
+    <?php
+      // variables to store words in silly story
+      $verb1 = "snorkeling";
+      $noun = "unicorn";
+      $adjective = "slimy";
+      $verb2 = "baked";
+
+      // display silly story with current silly words
+      echo "Last week I was $verb1 when all of a sudden I saw a $noun. 
+      It was $adjective. So I $verb2 it.";
+
+     ?>
+
+
+  </body>
+</html>
